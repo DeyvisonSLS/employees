b61a81f15a6c7d285e881243599ff6642f7e6b20
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-07-25 17:26:20 -0300

    For is implemented

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using employees.Entities;
 
 namespace employees
 {
@@ -6,7 +9,42 @@ namespace employees
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Employee> employeesList = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                //Is a outsourced?
+                Console.Write("Outsourced? (y/n)? ");
+                char outSourced = char.Parse(Console.ReadLine());
+                //Name
+                string name = Console.ReadLine();
+                //Hours
+                int hours = int.Parse(Console.ReadLine());
+                //Value per hour
+                double valuePerHour = double.Parse(Console.ReadLine());
+                
+                //Declaring Employee variable
+                Employee employee;
+
+                if(outSourced == 'y')
+                {
+                    //Pick the fourth value of the type
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine());
+                    //Then add to the list
+                    employee = new OutsourceEmployee(name, hours, valuePerHour, additionalCharge);
+                }
+                else
+                {
+                    employee = new Employee(name, hours, valuePerHour);
+                }
+                //Finally add to the list
+                employeesList.Add(employee);
+            }
+            
         }
     }
 }
