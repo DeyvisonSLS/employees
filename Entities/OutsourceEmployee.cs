d03614e89dc73ac8e1fdc59f6c1794425a86058e
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-07-25 17:26:57 -0300

    Subclass OutsourceEmployee is implemented

--- Entities/OutsourceEmployee.cs
+++ Entities/OutsourceEmployee.cs
@@ -1,8 +1,22 @@
 using System;
+using employees.Entities;
 
 namespace employees.Entities
 {
-    class Employees{
-
+    class OutsourceEmployee : Employee
+    {
+        double AdditionalCharge { get; set;}
+        public OutsourceEmployee()
+        {
+        }
+        public OutsourceEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+        public override double Payment()
+        {
+            double payment = base.Payment();
+            return payment + payment * 1.1;
+        }
     }
 }
