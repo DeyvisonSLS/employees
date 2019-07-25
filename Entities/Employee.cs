d499b3b5fefbbf5343f86fe9a6cc58fcee5b8b57
Deyvison Silas <deyvisonsilas122@gmail.com>
2019-07-25 17:26:39 -0300

    Superclass Employee is implemented

--- Entities/Employee.cs
+++ Entities/Employee.cs
@@ -2,7 +2,23 @@ using System;
 
 namespace employees.Entities
 {
-    class OutsourceEmployee{
-        
+    class Employee
+    {
+        public string Name { get; protected set; }
+        public int Hours { get; protected set; }
+        public double ValuePerHour { get; protected set; }
+        public Employee()
+        {
+        }
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+        public virtual double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
     }
 }
